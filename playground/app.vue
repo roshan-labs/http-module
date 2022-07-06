0ea49a3a91bfaeafd1af7bb53dbd8ce780d6b58b
gxmari007 <392041299@qq.com>
2022-07-06 21:51:36 +0800

    fix: `$http` 无类型

--- playground/app.vue
+++ playground/app.vue
@@ -5,5 +5,5 @@
 <script lang="ts" setup>
 const { $http } = useNuxtApp()
 
-const { data } = $http.useRequest('https://api.github.com/users/ruanyf')
+const { data } = $http.useRequest('https://api.github.com/users/gxmari007')
 </script>
