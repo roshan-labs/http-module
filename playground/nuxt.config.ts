7ea399aab73272975214963551ccb16bff3e8408
James Gao <392041299@qq.com>
2023-01-12 19:45:17 +0800

    chore: 更新依赖

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,6 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
-  ssr: false,
   modules: ['@roshan-labs/http-module'],
   http: {
     baseURL: 'https://api.github.com/',
